c8f0268872c455f2b3c4737dcd2437e28d18f849
unknown <bibekshr777@gmail.com>
2022-02-21 21:31:44 +0545

    final push with testing

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
-
+from django.test import TestCase, SimpleTestCase, Client
+from django.urls import reverse, resolve
+from app import views
+from app.views import *
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_case_index_url(self):
+        url=reverse('address')    
+        self.assertEquals(resolve(url).func,address)
+
+    def test_case_orders_url(self):
+        url=reverse('orders')    
+        self.assertEquals(resolve(url).func,orders)
+
+    def test_case_showcart_url(self):
+        url=reverse('showcart')    
+        self.assertEquals(resolve(url).func,show_cart)
+
+
+    def test_case_device_url(self):
+        url=reverse('device')    
+        self.assertEquals(resolve(url).func,mobile)
+
+
+    def test_case_checkout_url(self):
+        url=reverse('checkout')    
+        self.assertEquals(resolve(url).func,checkout)
+
+class Testviews(TestCase):
+    def test_views_indbhex(self):
+        client=Client()
+        url=reverse('app:ProductDetailView')
+        response=client.post(url)
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,"productdetail.html")
+
+
